98257f9cdd7e356ee1e88438cf9bb8ddfd807022
Fernando Bermudez <fernando.bermudez1@upr.edu>
2019-03-09 11:42:09 -0400

    Finished Town Walls
    Still needs to add dynamic entity into town, and needs to remove random
    light statue that appeared in the town

--- src/Game/World/InWorldAreas/TownArea.java
+++ src/Game/World/InWorldAreas/TownArea.java
@@ -93,18 +93,33 @@ public class TownArea extends BaseArea{
 		townWalls.add(new InWorldWalls(handler, 4250, 1970, 105, 204, "Wall"));	
 		townWalls.add(new InWorldWalls(handler, 4390, 1360, 850,850 , "Wall"));	
 		townWalls.add(new InWorldWalls(handler, 0, 4000, imageWidth, 10, "Wall"));	
-//		townWalls.add(new InWorldWalls(handler, 100, 0, 10, imageWidth, "Wall"));	
-//		townWalls.add(new InWorldWalls(handler, 100, 0, 10, imageHeight, "Wall"));	
-//		townWalls.add(new InWorldWalls(handler, 100, 0, 10, imageHeight, "Wall"));	
-//		townWalls.add(new InWorldWalls(handler, 100, 0, 10, imageHeight, "Wall"));	
-//		townWalls.add(new InWorldWalls(handler, 100, 0, 10, imageHeight, "Wall"));	
-//		townWalls.add(new InWorldWalls(handler, 100, 0, 10, imageHeight, "Wall"));	
-//		townWalls.add(new InWorldWalls(handler, 100, 0, 10, imageHeight, "Wall"));	
-//		townWalls.add(new InWorldWalls(handler, 100, 0, 10, imageHeight, "Wall"));	
-//		townWalls.add(new InWorldWalls(handler, 100, 0, 10, imageHeight, "Wall"));	
-//		townWalls.add(new InWorldWalls(handler, 100, 0, 10, imageHeight, "Wall"));	
-//		townWalls.add(new InWorldWalls(handler, 100, 0, 10, imageHeight, "Wall"));	
-//		townWalls.add(new InWorldWalls(handler, 100, 0, 10, imageHeight, "Wall"));	
+		townWalls.add(new InWorldWalls(handler, 5250, 0, 10, imageHeight, "Wall"));	
+		townWalls.add(new InWorldWalls(handler, 4800, 3070, 450, 15, "Wall"));	
+		townWalls.add(new InWorldWalls(handler, 4750, 3070, 10, 150, "Wall"));	
+		townWalls.add(new InWorldWalls(handler, 4550, 3210, 250, 15, "Wall"));	
+		townWalls.add(new InWorldWalls(handler, 4550, 3210, 15, 550, "Wall"));	
+		townWalls.add(new InWorldWalls(handler, 4250, 3710, 310, 15, "Wall"));	
+		townWalls.add(new InWorldWalls(handler, 4210, 3710, 10, 250, "Wall"));	
+		townWalls.add(new InWorldWalls(handler, 4052, 3860, 150, 250, "Wall"));	
+		townWalls.add(new InWorldWalls(handler, 1550, 3390, 910, 800, "Wall"));	
+		townWalls.add(new InWorldWalls(handler, 0, 2440, 1450, 720, "Wall"));	
+		townWalls.add(new InWorldWalls(handler, 1670, 2230, 150, 650, "Wall"));	//Middle of house
+		townWalls.add(new InWorldWalls(handler, 1420, 2303,350, 580, "Wall"));	//Left of house
+		townWalls.add(new InWorldWalls(handler, 1820, 2303, 320, 580, "Wall"));	//Right of house
+		townWalls.add(new InWorldWalls(handler, 2110, 2880, 150,150, "Wall")); //*Light
+		townWalls.add(new InWorldWalls(handler, 2180, 2680, 55,350, "Wall")); //**Post
+		townWalls.add(new InWorldWalls(handler, 2180, 2590, 55,150, "Wall")); //**next to house**//	
+		townWalls.add(new InWorldWalls(handler, 670, 3190, 15, 550, "Wall"));
+		townWalls.add(new InWorldWalls(handler, 670, 3710, 950, 15, "Wall"));	
+		townWalls.add(new InWorldWalls(handler, 2770, 2830, 1300, 530, "Wall"));
+		townWalls.add(new InWorldWalls(handler, 4010, 3210, 150, 150, "Wall"));
+		townWalls.add(new InWorldWalls(handler, 4110, 3010, 15, 350, "Wall"));
+		townWalls.add(new InWorldWalls(handler, 4070, 2890, 100, 150, "Wall"));	
+		townWalls.add(new InWorldWalls(handler, 3570, 2720, 250, 150, "Wall"));	//Middle House 2 
+		townWalls.add(new InWorldWalls(handler, 3370, 2760, 450, 150, "Wall"));	//Left House 2
+		townWalls.add(new InWorldWalls(handler, 3770, 2760, 250, 100, "Wall"));	//Right House 2
+		townWalls.add(new InWorldWalls(handler, 2780, 2220, 370, 350, "Wall"));	
+		townWalls.add(new InWorldWalls(handler, 3150, 2220, 350, 150, "Wall"));	
 		townWalls.add(new InWorldWalls(handler, 15,2025, 100, 300, "Exit"));	// Exit at Start
       						
 
