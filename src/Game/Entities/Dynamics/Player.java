8d2a0a0f40ad0512eb9e68ba90a0364cd09e6e7e
Fernando Bermudez <fernando.bermudez1@upr.edu>
2019-03-10 00:02:04 -0400

    Disabled Skill
    CHECKFORSKILL NEEDS FIXING

--- src/Game/Entities/Dynamics/Player.java
+++ src/Game/Entities/Dynamics/Player.java
@@ -444,7 +444,7 @@ public class Player extends BaseDynamicEntity implements Fighter {
 	double health = 200, mana = 100, xp = 0, lvl = 1, defense = 16, str = 10, intl = 25, mr = 12, cons = 20, acc = 12, evs = 4,
 			initiative = 13, maxHealth = 200, maxMana = 100, lvlUpExp = 200;
 
-	String Class = "none", skill = "Freeze";
+	String Class = "none", skill = "none";
 	String[] buffs = {}, debuffs = {};
 	
 
